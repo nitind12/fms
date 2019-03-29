5be47190e00f2282c41898d4baa385512bd6e106
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-03-29 22:26:55 +0530

    useless updates..:D
    please dont include dis 1

--- application/models/receipt_model.php
+++ application/models/receipt_model.php
@@ -7,5 +7,13 @@ function getclasses_sessionwise($session){
  $this->db->where('session_id',$session);
  $query=$this->db->get('class_in_session');
  echo $this->db->last_query();
-}
+	}
+
+
+/*function getclasses_studentwise($student){
+ $this->db->where('student_id',$student);
+ $query=$this->db->get('student_details');
+ echo $this->db->last_query();
+	}
+}*/
 }
