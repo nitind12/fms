51a00e1a4364c8db044a3aca0829cc14aa6c4562
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-04-04 13:24:29 +0530

    updates
    receipt

---
+++ application/views/testing/prnreceipt.php
@@ -0,0 +1,31 @@
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>Home
+				</a></li>
+				<li class="active">Registration</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Staff</h1>
+			</div>
+		</div>
+		<div class="panel-heading">Staff Details</div>
+		<div class="panel-body">
+		<div class="col-sm-6">
+			<form>
+				<table class="table table-striped">
+					<label>Regi No.:</label>
+					
+				</div>
+			</form>
+
+
+			<div class="col-sm-12">
+				<p class="back-link">BCA final Year Project by <a href="#">FMS team</a></p>
+			</div>
+		</div><!--/.row-->
+	</div>
