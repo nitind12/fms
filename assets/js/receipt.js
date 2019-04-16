d916721fadf8c6d91f56f16c9e5a6b499baa6a32
Kanika Mahtolia <kanikamahtolia9719@gmail.com>
2019-04-16 19:33:02 +0530

    update

--- assets/js/receipt.js
+++ assets/js/receipt.js
@@ -15,26 +15,37 @@ $(function(){
 				var str = '';
 				str = str + '<table class="table table-bordered table-hover table-responsive">';
 				str = str + '<tr>';
-				str = str + '<td>Reg. No.</td>';
+				str = str + '<td>Reg. No.:</td>';
 				str = str + '<td>'+obj.students['student_ID']+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Name</td>'
+				str = str + '<td>Name:</td>'
 				str = str + '<td>'+obj.students['first_Name']+' '+obj.students['last_Name']+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Class</td>'
+				str = str + '<td>Class:</td>'
 				str = str + '<td>'+obj.students['class_sess_ID']+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Session</td>'
+				str = str + '<td>Session:</td>'
 				str = str + '<td>'+obj.students['session_ID']+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Father</td>'
+				str = str + '<td>Father:</td>'
 				str = str + '<td>'+obj.students['fathers_Name']+'</td>';
 				str = str + '</tr>';
 				str = str + '</table>';
+				str = str + '<table class="table table-bordered table-hover table-responsive">';
+				str = str + '<tr>';
+				str = str + '<td>Fee Details Below:</td>';
+				str = str + '</tr>';
+				str = str + '<td>Actual Fee:</td>';
+				str = str + '<tr>';
+				str = str + '<td>Previous Due:</td>';
+				str = str + '</tr>';
+				str = str + '<td>Total Due:</td>';
+				str = str + '</tr>';
+
 
 				$('#invoicedatahere').html(str);
 			
