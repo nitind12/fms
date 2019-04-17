235d4c2f11a7407697c3edeef80e27b7b432b984
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-04-17 12:54:45 +0530

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,22 +1,11 @@
 <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>"></script>
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=7.2"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=7.5"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=1.0"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=1.3"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.7"></script>
-
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=5.6"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.2"></script>
-
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=4.8"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.0"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.2"></script>
-
-
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=5.6"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.0"></script>
 
 
 	<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
