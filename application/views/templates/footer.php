03bed7a82ad220e112162030a46851cf1a104f9b
Kanika Mahtolia <kanikamahtolia9719@gmail.com>
2019-04-20 00:32:57 +0530

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,10 @@
 <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>"></script>
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.1"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=7.2"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=1.0"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.3"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.7"></script>
 
 
@@ -14,11 +13,11 @@
 
 
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=7.6"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.2"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.3"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.7"></script>
 
 
--- assets/js/receipt.js
+++ assets/js/receipt.js
@@ -18,9 +18,9 @@ $(function(){
 				var str = '';
 				str= str + '<div class=" col-sm-offset-3 col-lg-8 col-lg-offset-3 main dotted">'; 
 				str= str + '<table><tr>';
-				str = str + '<td align="left" style="width: 150px;font-size: 13px; padding: 0px 8px 0px 0px; vertical-align: middle">Date: 27/2/2019</span></td>'; 
+				str = str + '<td align="left" style="width: 150px;font-size: 13px; padding: 0px 8px 0px 0px; vertical-align: middle"><b>Date: 27/2/2019</b></span></td>'; 
 				str = str + '<td align="center" style="width: 500px;padding: 0px 0px 0px 8px; vertical-align: middle"><h4 align="center"><b>The Demo School</b><br>Receipt</h4></td>';
-				str = str + '<td align="right" style="width: 150px;font-size: 13px; padding: 0px 8px 0px 0px; vertical-align: middle">Receipt No.</td>'; 
+				str = str + '<td align="right" style="width: 150px;font-size: 13px; padding: 0px 8px 0px 0px; vertical-align: middle"><b>Receipt No.</b></td>'; 
 				str = str + '</tr>';
 				str = str + '<div class="row"';
 				str = str + '<div class="col-md-6">';
@@ -57,17 +57,18 @@ $(function(){
 				str = str + '<table><b><u class="text-danger">Fee Details Below:</u></b>'
 				str = str + '</tr>';
 				str = str + '<table class="table table-bordered table-hover table-responsive">';
-				str = str + '<tr>'
-				str = str + '<td>Actual Fee:</td>';
-				str = str + '<td>'+obj.students['actual_Amount']+'</td>';
+				str = str + '<tr>';
+				str = str + '<td style="background: #f0f0f0; color: #900000"><b>Actual Fee:</b></td>';
+
+				str = str + '<td style="background: #f0f0f0; color: #900000">Rs.&nbsp&nbsp'+obj.students['actual_Amount']+'</td>';
 				str = str + '</tr>'
 				str = str + '<tr>';
-				str = str + '<td>Previous Due:</td>';
-				str = str + '<td>'+obj.students['actual_due_Amount']+'</td>';
+				str = str + '<td style="background: #f0f0f0; color: #900000"><b>Previous Due:</b></td>';
+				str = str + '<td style="background: #f0f0f0; color: #900000">Rs.&nbsp&nbsp'+obj.students['actual_due_Amount']+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Total Due:</td>';
-				str = str + '<td>'+obj.students['due_Amount']+'</td>';
+				str = str + '<td style="background: #f0f0f0; color: #900000"><b>Total Due:</b></td>';
+				str = str + '<td style="background: #f0f0f0; color: #900000"><b>Rs.&nbsp&nbsp</b>'+obj.students['due_Amount']+'</td>';
 				str = str + '</tr>';
 				str = str + '</table>';
 				str = str + '</table>';
@@ -80,36 +81,44 @@ $(function(){
 				total='';
 			    str = str + '<table class="table table-bordered table-hover table-responsive">';
 			    str = str + '<tr>';
-				str = str + '<td>Total Due:</td>';
+				str = str + '<td>Total Due: <span style="float: right; padding: 8px 0px; font-size: 11px" class="fa fa-plus"></span></td>';
 				due_Amount=obj.students['due_Amount'];
-				str = str + '<td>'+due_Amount+'</td>';
+				str = str + '<td>Rs.&nbsp&nbsp'+due_Amount+'</td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Discount?</td>';
-				discount_Amount=obj.discount['discount_Amount'];
-				str = str + '<td>'+discount_Amount+'</td>';
+				str = str + '<td style="color: #909000">Discount? <span style="float: right; padding: 8px 0px; font-size: 11px" class="fa fa-minus"></span><div style="float: left; font-size: 8px; color: #0000ff; clear: both"></div></td>';
+				str = str + '<td>Rs.<input type="text" id="_discount_" name="_discount_" value="0" style="width: 100px; padding: 0px; background: #f0f000; border:#000000 solid 0px"></td>';
 				str = str + '</tr>';
 				str = str + '<tr>';
-				str = str + '<td>Fine?</td>';
-				fine=obj.discount['fine'];
-				str = str + '<td>'+fine+'</td>';
+				str = str + '<td style="color: #909000">Fine? <span style="float: right; padding: 8px 0px; font-size: 11px" class="fa fa-plus"></span><div style="float: left; font-size: 8px; color: #0000ff; clear: both"></div></td>';
+				str = str + '<td>Rs.<input type="text" id="_fine_" name="_fine_" value="0" style="width: 100px; padding: 0px; background: #f0f000; border:#000000 solid 0px"></td>';
+				
 				str = str + '</tr>';
 				
 				str = str + '<tr>';
 				str = str + '<td>Total:</td>';
-				total=parseInt(due_Amount)-parseInt(discount_Amount)+parseInt(fine);
+				
 
-				str = str + '<td>'+total+'</td>';
+				str = str + '<td>';
+				str = str + '</td>';
 				str = str + '</tr>';
-				str = str + '<tr>';
+				
 				str = str + '</table>';
 				str = str + '<button type="button" class="btn btn-danger">Update</button>';
 				str = str + '<table class="table table-bordered table-hover table-responsive">';
-				str = str + '<td width="200px" style="background: #406373; color: #ffffff">Paid Amount</td>';
-				total=parseInt(due_Amount)-parseInt(discount_Amount)+parseInt(fine);
+				str = str + '<tr>';
+				str = str + '<td width="200px" style="background: #406373; color: #ffffff">Paid Amount </td>';
+ 				str = str + '<td width="160px" style="background: #406373; color: #ffffff"><label class="receipt_label">: Rs. </label><span class="receipt_content"><input type="text" id="paid_amount" name="paid_amount" placeholder="" value="" style="width: 100px; padding: 0px; border:#f0f0f0 solid 1px; color: #0000ff; font-weight: bold">/-</span></td>';
+ 				str = str + '</tr>';
+
+				/*str = str + '<div class="form-group">';
+			    str = str + '<label for="usr">Paid Amount (Rs.)</label>';
+ 				str = str +  '<input type="text" class="form-control" id="usr">';
+				/*str = str + '</div><td>td width="200px" style="background: #406373; color: #ffffff">Paid Amount</td>';*/
+				str = str + '</table>';
 				
-				str = str + '<td <td width="160px" style="background: #406373; color: #ffffff">'+total+'</td>';
-				str = str + '</td>';
+				
+				
 
 				
 				str = str + '<table class="table table-bordered table-hover table-responsive">';
