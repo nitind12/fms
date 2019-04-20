31de42f48e6c3c038b42cb7e0546fc63c7740765
charujoshi50 <49025369+charujoshi50@users.noreply.github.com>
2019-04-20 15:47:36 +0530

    Update classes.php

--- application/controllers/classes.php
+++ application/controllers/classes.php
@@ -10,8 +10,8 @@ function __construct(){
 
 	public function index()
 	{
-		$this->load->model('student_model','stm')
-		$data['course'] = $this->stm->getCourse();
+		$this->load->model('class_model','cm');
+		$data['course'] = $this->cm->getCourse();
 
 		$this->load->view('templates/header');
 		$this->load->view('templates/navheader');
