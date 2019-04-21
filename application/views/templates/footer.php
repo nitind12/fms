c61c756a4febc7c87c4cdf8c3470b60c1892f424
Kanika Mahtolia <kanikamahtolia9719@gmail.com>
2019-04-21 19:54:11 +0530

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,21 +1,11 @@
 <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>"></script>
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.7"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.8"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.8"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoiceprint.js');?>?version=1.9"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=1.8"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=8.2"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=1.2"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.4"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=2.4"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=3.1"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoiceprint.js');?>?version=2.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.7"></script>
 
 
