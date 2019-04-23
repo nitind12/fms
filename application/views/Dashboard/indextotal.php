9d5a5ab7dffd579ad87ce52f7ec5669bed47a14e
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-04-23 12:36:59 +0530

    dashboard

---
+++ application/views/Dashboard/indextotal.php
@@ -0,0 +1,21 @@
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>Home
+				</a></li>
+				<li class="active">Total Students</li>
+			</ol>
+		</div><!--/.row-->
+		<div id="ttlstdnts" style="clear: both"></div>
+		
+		
+				
+								
+		
+			
+			<div class="col-sm-12">
+				<p class="back-link">BCA final Year Project by <a href="#">FMS team</a></p>
+			</div>
+		</div><!--/.row-->
+	</div>
