499566010eef525d41e6dec129419146e8f46982
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-05-03 13:58:22 +0530

    jdbcug

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>"></script>
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=15.3"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=15.4"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=4.8"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.5"></script>
