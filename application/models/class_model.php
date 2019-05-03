84e0c140141cceab9c5a0c7c0e3fc9669978f7a9
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-05-03 11:58:16 +0530

    master

--- application/models/class_model.php
+++ application/models/class_model.php
@@ -53,19 +53,10 @@ class class_model extends CI_Model {
 			//$this->db->select('b.course,a.course_ID,a.sem_ID,a.section');
 			$this->db->from('class a');
 			$this->db->join('course_details b', 'a.course_ID=b.course_ID');
-			$this->db->join('student_in_session c', 'a.class_ID=c.class_ID');
-			$this->db->group_by('c.class_ID');
 			$query= $this->db->get();
 
 			return $query->result();
 		}
-		/*function getclass_invoice(){
-			$this->db->from('class_in_session a');
-			$this->db->join('student_in_session b', 'a.class_sess_ID=b.class_sess_ID');
-			$query= $this->db->get();
-
-			return $query->row();
-		}*/
 
 		function getClassesStudentwise(){
 			$this->db->select('a.*, b.course, count(c.student_ID) as totalStudents');
