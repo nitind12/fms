2e117f204db7fcfd0dcbac6728bd3d9036ed87c3
Kanika Mahtolia <kanikamahtolia9719@gmail.com>
2019-05-04 16:19:22 +0530

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,14 +1,12 @@
 <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>"></script>
 
-<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=15.6"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/invoice.js');?>?version=16.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript2.js');?>?version=1.1"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript3.js');?>?version=5.0"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/myscript.js');?>?version=6.5"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=10.7"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/receipt.js');?>?version=11.5"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.8"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/class_in_session.js');?>?version=2.1"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/session.js');?>?version=1.7"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/js/class_in_session.js');?>?version=2.0"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/class.js');?>?version=2.4"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/student.js');?>?version=1.9"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/invoicedisplay.js');?>?version=1.4"></script>
