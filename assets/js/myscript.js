4065d8c5f6011ec101f84f36b221e533a2d0e9ca
Maneeeeesh <43409927+Maneeeeesh@users.noreply.github.com>
2019-05-07 07:16:40 +0530

    myscrip update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -10,39 +10,42 @@ $(function(){
 			url: url_,
 			success: function(data){
 				//alert(data);	
-				var obj=JSON.parse(data);			
+				var obj=JSON.parse(data);	
 
-				$('#txtfrstnme').val(obj['stud']['first_Name']);
-				$('#txtlstnme').val(obj['stud']['last_Name']);
-				$('#txtfnme').val(obj['stud']['fathers_Name']);
-				$('#txtdte').val(obj['stud']['dob']);
-				$('#txtntnlty').val(obj['stud']['nationality']);
-				$('#txtbgrp').val(obj['stud']['blood_Group']);
-				$('#txtstts').val(obj['stud']['status']);
-				$('#txtmnme').val(obj['stud']['mothers_Name']);
-				$('#txtusrnme').val(obj['stud']['username']);
-				$('#txtctgry').val(obj['stud']['category']);
-				$('#txtcntct').val(obj['stud']['contact']);
-				$('#txtemail').val(obj['stud']['email']);
-				$('#txtaddr').val(obj['stud']['address']);
-				$('#txtarea').val(obj['stud']['area']);
-				$('#txtcity').val(obj['stud']['city']);
-				$('#txtstate').val(obj['stud']['state']);
-				$('#txtdstrct').val(obj['stud']['district']);
-				$('#txtpincode').val(obj['stud']['pincode']);
-				$('#txtsibling').val(obj['stud']['siblings']);
-				$('#txtdoa').val(obj['stud']['date']);	
-				$('#txtdisc').val(obj['stud']['discount_offered']);
-				$('#disc').val(obj['stud']['discount_on']);
-				$('#txtcourse').val(obj['stud']['course_ID']);
+				if(obj['stud']['student_ID'] == ''){}		
+				else{
+					$('#txtfrstnme').val(obj['stud']['first_Name']);
+					$('#txtlstnme').val(obj['stud']['last_Name']);
+					$('#txtfnme').val(obj['stud']['fathers_Name']);
+					$('#txtdte').val(obj['stud']['dob']);
+					$('#txtntnlty').val(obj['stud']['nationality']);
+					$('#txtbgrp').val(obj['stud']['blood_Group']);
+					$('#txtstts').val(obj['stud']['status']);
+					$('#txtmnme').val(obj['stud']['mothers_Name']);
+					$('#txtusrnme').val(obj['stud']['username']);
+					$('#txtctgry').val(obj['stud']['category']);
+					$('#txtcntct').val(obj['stud']['contact']);
+					$('#txtemail').val(obj['stud']['email']);
+					$('#txtaddr').val(obj['stud']['address']);
+					$('#txtarea').val(obj['stud']['area']);
+					$('#txtcity').val(obj['stud']['city']);
+					$('#txtstate').val(obj['stud']['state']);
+					$('#txtdstrct').val(obj['stud']['district']);
+					$('#txtpincode').val(obj['stud']['pincode']);
+					$('#txtsibling').val(obj['stud']['siblings']);
+					$('#txtdoa').val(obj['stud']['date']);	
+					$('#txtdisc').val(obj['stud']['discount_offered']);
+					$('#disc').val(obj['stud']['discount_on']);
+					$('#txtcourse').val(obj['stud']['course_ID']);
 
 
-				if(obj['stud']['gender'] == 'MALE'){
-				
+					if(obj['stud']['gender'] == 'MALE'){
+					
 					$('#txtmle').prop('checked', true);
 				} else {
 					$('#txtfmle').prop('checked', true);
 				}
+			}
 
 			}
 		});
