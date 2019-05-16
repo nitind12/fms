3a710bbdf647be01518219016e8c2b9d962de99c
LalitSinghRathore <43352002+LalitSinghRathore@users.noreply.github.com>
2019-05-16 13:29:07 +0530

    Update index_static_in_class.php

--- application/views/Dashboard/index_static_in_class.php
+++ application/views/Dashboard/index_static_in_class.php
@@ -1,81 +1,96 @@
-    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-        <div class="row">
-            <ol class="breadcrumb">
-                <li><a href="#">
-                    <em class="fa fa-home"></em>Home
-                </a></li>
-                <li class="active">Master</li>
-            </ol>
-        </div><!--/.row-->
-        
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Static In Class</h1>
-            </div>
-            <ul class="pagination">
-                <li><a data-toggle="tab" href="#mfhead">Manage Static Heads</a></li>
-                <li><a data-toggle="tab" href="#afhead">Associate Static Heads</a></li>
-                
-            </ul>
-            <div class="tab-content">
-                <div id="mfhead" class="tab-pane fade">
-                                <div class="panel-heading">Add Static Heads<em class="caret"></em></div>
-                                <div class="panel-body ">
-                    <form role="form" name="frmSession" method="post" action="<?php echo site_url('statichead_students/insert_record');?>">
-                                <!--student details-->
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                    <label>New Static Head</label>
-                                    <input class="form-control"name="txtsth" id="txtsth">
-                                </div></div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                    <label>Amount</label>
-                                    <input class="form-control" name="txtsthamt" id="txtsthamt">
-                                </div>
-                            </div>
-                                <div class="form-group col-md-3">
-                                        <label>How Many Times</label>
-                                        <select class="form-control" name="txthmt" id="txthmt">
-                                            <option>Select</option>
-                                            <option>One Time</option>
-                                            <option>As Per Selected Months</option>
-                                        </select><br>
-                                    </div>
-                           <div class="col-sm-12">
-                                    <button type="submit" class="btn btn-success">Add</button>
-                                
-                            </div>
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+      <ol class="breadcrumb">
+        <li><a href="#">
+          <em class="fa fa-home"></em>Home
+        </a></li>
+        <li class="active">Master</li>
+      </ol>
+    </div><!--/.row-->
+    
+      <div class="col-lg-12">
+        <h1 class="page-header">Static in class</h1>
+      </div>
+      <ul class="pagination">
+        <li class="active"><a data-toggle="tab" href="#mshead">Manage Static Heads</a></li>
+        <li><a data-toggle="tab" href="#ashead">Associate Static Heads</a></li>
+        </ul>
+         <div class="tab-content">    
+        <div id="mshead" class="tab-pane fade in active">
 
-                            <div class="form-group">
-                                    <div class="col-md-12 widget-left" id="msghere" style="font-size: 10px; color: #ff0000">
-                                    <br><br>    <?php echo $this->session->flashdata('msg_'); ?>
-                                    </div>
-                                </div>
-                        </form>
+          <div class="panel-heading">Add static Heads</div>
+          <table class="table table-bordered">
+            <tr>
+              <th>
+                <div class="panel-body ">
+                  <form class="form form-vertical" name="frm" action="<?php echo site_url('flexihead_students/insert_record');?>" method="post" id="frm">
+                     <div class="form-group ">
+                       <label>New static Head</label>
+                         <input class="form-control"name="txtffh" id="txtffh">
+                    </div>
+                    <div class="form-group">
+                      <label>Amount</label>
+                        <input class="form-control" name="txtffhamt" id="txtffhamt">
+                    </div>
+                  
+                <div class="form-group">
+                    <label>How Many Times</label>
+                    <select class="form-control" name="txthmt" id="txthmt">
+                      <option>Select</option>
+                      <option>One Time</option>
+                      <option>As Per Selected Months</option>
+                    </select><br>
+                  </div>
+                  <div class="col-sm-12">
+                  <button type="submit" class="btn btn-success">Add</button>    
+              </div>
+
+              <div class="form-group">
+                  <div class="col-md-12 widget-left" id="msghere" style="font-size: 10px; color: #ff0000">
+                  <br><br>  <?php echo $this->session->flashdata('msg_'); ?>
+                  </div>
+                </div>
+            </form>
 
             </div>
-                         <div class="controls span6">
+          </div>
+        </th>
+          <th>
+             <div class="controls span6">
         <div class="widget-box">
             
             <div class="widget-content nopadding">
                 <div class="">View Static Head<em class="caret"></em></div>
-              <table>
+                <table>
                   <tr>
-                    <th><div style="clear: both;" id= "printstaticdata"></div></th>
-                    <th> <div style="clear: both;" id= "printstaticdata1"></div> <div style="clear: both;" id= "ptrn"></div>  </th>
+                    <th><div style="clear: both;" id= "printheres"></div></th>
+                    <th> <div style="clear: both;" id= "printheres1"></div> <div style="clear: both;" id= "ptrn"></div>  </th>
                  </tr>
                </table>
+                
             </div>
         </div>
     </div>
-    <div class="controls span4">
+  </th>
+  </tr>
+</table>
+</div>
+
+
+    
+    <div id="ashead" class="tab-pane fade">
+
+
+
+
+
+<div class="controls span4">
     <div class="widget-box">
             <form class="form" id="frmStaticheadData">
 
             <div  style="border: #ff0000 solid 0px; width: 50px; height:50px; float: right; right: 0px; z-index: 2222; position: absolute;" id="student_photo_here"></div>
             <div class="widget-title"> <span class="icon"> <i class="icon-ok-sign"></i> </span>
-                <h5>Static Heads</h5>
+                <h3>Static Heads</h3>
             </div>
             <div class="widget-content">
                 <div class="control-group">
@@ -139,8 +154,9 @@
                 
               </tbody>
             </table>
-          </form>
-          </div>
-        </div>
-    </div>
-    
+
+
+
+       <div class="col-sm-12">
+        <p class="back-link">BCA final Year Project by <a href="#">FMS team</a></p>
+      </div>
